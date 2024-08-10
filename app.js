6ce16dc5190a9857720552d5462a3fdc801efeaa
Shameer Ebrahim <shameer66ster@gmail.com>
2024-08-10 19:51:06 +0530

    added controllers

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is listning to Port ${port}`);
+});
